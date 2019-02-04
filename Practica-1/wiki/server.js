4bc6f4620b2a0e3e3e2d8cc30cf9dd3280473f3b
prodaromero <elrodas95@gmail.com>
2019-02-04 14:17:31 +0100

    Server mejorado

--- Practica-1/wiki/server.js
+++ Practica-1/wiki/server.js
@@ -1,10 +1,15 @@
 var http = require('http');
 
-console.log("Arrancando servidor...")
+console.log("Arrancando servidor...");
 
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-  console.log("Peticion atendida")
+server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+    console.log("---> Petición recibida");
+    console.log("--> Cabecera de la solicitud: ");
+    //console.log(req.headers);
+    console.log("HOST: " + req.headers.host);
+    console.log("CONNECTION: " + req.headers.connection);
+    console.log("USER AGENT: " + req.headers['user-agent']);
+    console.log("Recurso solicitado (URL): " + req.url);
 }).listen(8080);
