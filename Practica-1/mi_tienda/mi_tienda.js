3c57fc44a2e9b66bebb71e77e4f1256b1a257750
prodaromero <elrodas95@gmail.com>
2019-04-01 20:15:45 +0200

    Añadimos cositas

--- Practica-1/mi_tienda/mi_tienda.js
+++ Practica-1/mi_tienda/mi_tienda.js
@@ -8,55 +8,34 @@ console.log("Local-server listening at port 8080...");
 //-- Configuration and lunch the server.
 http.createServer((req, res) => {
 
-    console.log("---> Reques recived");
-    console.log("Requested resource (URL): " + req.url);
+    var ruta = url.parse(req.url, true);
 
-    var q = url.parse(req.url, true);
+    var recurso = "";
 
-    console.log("URL parseada: ");
-    console.log("Host: " + q.host);
-    console.log("Pathname: " + q.pathname);
-
-    //-- Obtain the file.
-
-    var filename = "";
-
-    if (q.pathname == "/") {
-        filename += "/index.html";
-    }else {
-        filename = q.pathname;
+    if (ruta.pathname == "/") {
+        recurso += "mi_tienda.html";
+    } else {
+        recurso = "." + ruta.pathname;
     }
 
-    //-- Obtain the second file
-    tipo = filename.split(".")[1];
+    var extension = recurso.split(".")[2];
 
-    //-- Get the filename form the requested resource
-    filename = "." + filename;
-
-    console.log("Filename: " + filename);
-    console.log("Tipo: " + tipo);
-
-    fs.readFile(filename, function(error, data) {
-        if (error) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
+    fs.readFile(recurso, function(err, data) {
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          return res.end("404 Not Found");
         }
 
-        var mime = "text/html";
+        //-- Tipo mime por defecto: html
+        var mime = "text/html"
 
-        if (['png','jpg'].includes(tipo)) {
-            console.log("IMAGEN!!!!");
-            mime = "image/" + tipo;
-        }
-
-        if (tipo == "css") {
+        if (extension == "css") {
             mime = "text/css";
         }
 
-        //-- Request message
-        res.writeHead(200, {'Content-Type':mime});
-        res-write(data);
+        //-- Generar el mensaje de respuesta
+        res.writeHead(200, {'Content-Type': mime});
+        res.write(data);
         res.end();
     });
-    
 }).listen(8080);
