6de89c845e03c57e86339b9ba9431b0244bf5192
prodaromero <elrodas95@gmail.com>
2019-04-30 19:56:16 +0200

    20190430

--- Practica-2/mi_proyectoweb/mi_tienda/urls.py
+++ Practica-2/mi_proyectoweb/mi_tienda/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns =  [
     url(r'^$', views.home_view),
-    url(r'^admin/', admin.site.urls),
     url(r'^index', views.home_view),
     url(r'^AvionesCombate', views.AvionesCombate_view),
+    url(r'^AvionesComerciales', views.AvionesComerciales_view),
 ]
