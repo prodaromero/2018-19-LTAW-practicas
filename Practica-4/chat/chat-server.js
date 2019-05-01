4f29ee8f93a7efc173ff51f97083dd0630140141
prodaromero <elrodas95@gmail.com>
2019-05-01 20:34:39 +0200

    20190501

---
+++ Practica-4/chat/chat-server.js
@@ -0,0 +1,47 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//--Servir la pagina principal
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/chat.html');
+  console.log("Página principal: /")
+});
+
+//-- Servir el cliente javascript
+app.get('/chat-client.js', function(req, res){
+  res.sendFile(__dirname + '/chat-client.js');
+  console.log("Fichero js solicituado")
+});
+
+app.get('/chat-style.css', function(req, res){
+  res.sendFile(__dirname + '/chat-style.css');
+  console.log("Fichero css solicituado")
+});
+
+//-- Lanzar el servidor
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+//-- Evento: Nueva conexion recibida
+//-- Un nuevo cliente se ha conectado!
+io.on ('connection', function(socket){
+    console.log('--> Usuario conectado!')
+
+    //-- Detectar si el usuario se ha desconectado
+    socket.on('disconnect', function(){
+        console.log('--> Usuario desconecado');
+    });
+
+    //-- Detectar si se ha recibido un mensaje del cliente
+    socket.on('new_message', msg => {
+        console.log("Mensaje recibido: " + msg);
+
+        //-- Notificarlo en la consola del servidor
+        console.log("Mensaje recibido: " + msg);
+
+        io.emit('new_message', msg);
+    });
+
+});
